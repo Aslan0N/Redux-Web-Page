7d14e13d630b85e6b4b56a3a932419e0efbc88e9
Aslan0N <thenzrv@gmail.com>
2023-04-14 21:37:19 +0400

    Redux Web Page

---
+++ assets/data/data.js
@@ -0,0 +1,46 @@
+const data = {
+    "english":
+    {
+        "ul":['Getting Started','Tutorial','Usage Guide','API','FAQ','Best Practices','GitHub','Need help?'],
+        "main":
+        {
+            "p":"A Predictable State Container for JS Apps",
+            "btn":"Get Started",
+            "icon":['Predictable','Centralized','Debuggable','Flexible'],
+            "descr":[
+                'Redux helps you write applications that behave consistently, run in different environments (client, server, and native), and are easy to test.',
+                "Centralizing your application's state and logic enables powerful capabilities like undo/redo, state persistence, and much more.",
+                'The Redux DevTools make it easy to trace when, where, why, and how your application\'s state changed. Redux\'s architecture lets you log changes, use "time-travel debugging", and even send complete error reports to a server.',
+                'Redux works with any UI layer, and has a large ecosystem of addons to fit your needs.'
+            ],
+            "team":"Other Libraries from the Redux Team",
+            "libraries":['Official React bindings for Redux','The official, opinionated, batteries-included toolset for efficient Redux development'],
+            "footerTitle": ['Docs','Community','More'],
+            "foterLink":['Get Started','Usage Guide','Tutorial','FAQ','API Reference','Reactiflux Discord','Stack Overflow','Feedback','GitHub'],
+            'copyRight':'Copyright © 2015–2023 Dan Abramov and the Redux documentation authors.'
+        }
+    },
+    "azerbaijan":
+    {
+        "ul":['Başlayarkən','Dərslik','İstifadə Təlimatı','API','FAQ','Təcrübələr','GitHub','Kömək Lazımdır?'],
+        "main":
+        {
+            "p":"JS Tətbiqləri üçün proqnozlaşdırıla bilən Dövlət Konteyneri",
+            "btn":"Başlayın",
+            "icon":['Proqnozlaşdırıla bilən','Mərkəzi','Yanlış','Çevik'],
+            "descr":[
+                "Redux ardıcıl davranan, müxtəlif mühitlərdə (müştəri, server və yerli) işləyən və sınaqdan keçirilməsi asan olan proqramlar yazmağa kömək edir.",
+                "Tətbiqinizin vəziyyətini və məntiqini mərkəzləşdirmək geri qaytarmaq/yenidən etmək, vəziyyətin davamlılığı və sair kimi güclü imkanlara imkan verir.",
+                "Redux DevTools tətbiqinizin vəziyyətinin nə vaxt, harada, niyə və necə dəyişdiyini izləməyi asanlaşdırır. Redux-un arxitekturası sizə dəyişiklikləri qeyd etməyə, \"zaman səyahətində sazlama\" istifadə etməyə və hətta tam səhv hesabatlarını serverə göndərməyə imkan verir.",
+                "Redux istənilən istifadəçi interfeysi təbəqəsi ilə işləyir və ehtiyaclarınıza uyğun əlavələrin böyük ekosisteminə malikdir."
+            ],
+            "team":"Redux komandasının digər kitabxanaları",
+            "libraries":['Redux üçün rəsmi reaksiya bağlamaları','Redux-un səmərəli inkişafı üçün rəsmi, rəyli, batareyalarla təchiz edilmiş alətlər dəsti'],
+            "footerTitle": ['Sənədlər','Icma','Daha Çox'],
+            "foterLink":['Başlayarkən','İstifadə Təlimatı','Dərslik','FAQ','API Arayışı','Reactiflux Discord','Stack Overflow','Rəy','GitHub'],
+            'copyRight':'Copyright © 2015–2023 Dan Abramov və Redux sənədlərininmüəllifləri.'
+        }
+    }
+}
+
+export {data};
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+import { data } from "../data/data.js"
 // Ul Open Menu
 const bars = document.querySelector(".fa-bars")
 const ul = document.querySelector("#menu-ul")
+const close = document.querySelector(".fa-x")
 
 const openMenu= ()=>{
     ul.classList.add("open")
@@ -8,6 +10,8 @@ const openMenu= ()=>{
 const closeMenu = ()=>{
     ul.classList.remove("open")
 }
+close.addEventListener('click',closeMenu)
+bars.addEventListener('click',openMenu)
 
 // Dark / Light Mode
 const body = document.querySelector("body");
@@ -37,3 +41,94 @@ const changeMode = ()=>{
         darkEnable()
     }
 }
+modeBtn.addEventListener('click',changeMode)
+
+// Change Language
+
+const langBtn = document.querySelector(".lang");
+const li = document.querySelectorAll(".nav-link");
+const libraries = document.querySelector(".lib");
+const libP = document.querySelectorAll(".lib-p");
+const title = document.querySelectorAll(".footer-title");
+const links = document.querySelectorAll(".footer-link")
+const end = document.querySelector(".end-p")
+const langAz = ()=>{
+    langBtn.innerHTML = "AZ"
+        localStorage.setItem("Language","Az")
+        for(let i in data.azerbaijan.ul){
+            li[i].innerHTML = data.azerbaijan.ul[i]
+        }
+        langBtn.removeAttribute('language')
+        langBtn.setAttribute('language','azerbaijan')
+        for(let j in data.azerbaijan.main.icon){
+            h2Cards[j].textContent = data.azerbaijan.main.icon[j]
+        }
+        for(let j in data.azerbaijan.main.descr){
+            cardsP[j].textContent = data.azerbaijan.main.descr[j]
+        }
+        p.textContent = data.azerbaijan.main.p
+        a.textContent = data.azerbaijan.main.btn
+        libraries.textContent = data.azerbaijan.main.team
+        for(let i in data.azerbaijan.main.libraries){
+            libP[i].textContent = data.azerbaijan.main.libraries[i]
+        }
+        for(let i in data.azerbaijan.main.footerTitle){
+            title[i].textContent = data.azerbaijan.main.footerTitle[i]
+        }
+        for(let i in data.azerbaijan.main.foterLink){
+            links[i].textContent = data.azerbaijan.main.foterLink[i]
+        }
+        end.textContent = data.azerbaijan.main.copyRight
+}
+const langEn = ()=>{
+    langBtn.innerHTML = "EN"
+    localStorage.setItem("Language","En")
+    for(let i in data.english.ul){
+        li[i].innerHTML = data.english.ul[i]
+    }
+    for(let j in data.english.main.icon){
+        h2Cards[j].textContent = data.english.main.icon[j]
+    }
+    for(let j in data.english.main.descr){
+        cardsP[j].textContent = data.english.main.descr[j]
+    }
+    p.textContent = data.english.main.p
+    a.textContent = data.english.main.btn
+    libraries.textContent = data.english.main.team
+    for(let i in data.english.main.libraries){
+        libP[i].textContent = data.english.main.libraries[i]
+    }
+    for(let i in data.english.main.footerTitle){
+        title[i].textContent = data.english.main.footerTitle[i]
+    }
+    for(let i in data.english.main.foterLink){
+        links[i].textContent = data.english.main.foterLink[i]
+    }
+    end.textContent = data.english.main.copyRight
+}
+const p = document.querySelector(".redux-p")
+const a = document.querySelector(".redux-a")
+const h2Cards = document.querySelectorAll(".two-sec-h")
+const cardsP = document.querySelectorAll(".two-sec-p")
+const langChange = ()=>{
+    if(langBtn.getAttribute == 'english'){
+        langBtn.setAttribute('language', 'azerbaijan')
+
+    }else{
+        langBtn.setAttribute('language', 'english')
+    }
+    let localLang = localStorage.getItem("Language")
+    if(localLang == "En"){
+        langAz()
+    }else{
+        langEn()
+    }
+    const attr = langBtn.getAttribute('language')
+    
+   
+}
+let localLang = localStorage.getItem("Language")
+if(localLang == "Az"){
+    langAz()
+}
+langBtn.addEventListener('click',langChange)
